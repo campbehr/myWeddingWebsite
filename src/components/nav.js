ad920d69a12f7f6d689b4bd9ee7e4ed2f346ee88
campbehr <hunter.campbell843@gmail.com>
2020-08-19 17:01:18 -0400

    working on nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,9 +1,16 @@
 import React from "react"
 import { Link } from "gatsby"
+import navStyles from "../styles/navStyles/navStyles.module.css"
 
 export default function Nav() {
   return (
-    <nav>
+    <nav className={navStyles.nav}>
+      <div className={navStyles.hamburger}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
       <ul>
         <Link to="/">About</Link>
         <Link to="/info/">Info</Link>
