4b4b3cb86e3c1f0acabf124bf0efbb3ef0a92879
campbehr <hunter.campbell843@gmail.com>
2020-08-21 17:02:40 -0400

    still working on nav

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,25 @@
 import React from "react"
 import { Link } from "gatsby"
+import "../styles/fonts.css"
+import styled from "styled-components"
+
+const Title = styled.h1`
+  font-family: "Allura", cursive;
+  color: #9dccc9;
+  font-size: 5em;
+  margin: 0.5em;
+
+  @media screen and(min-width: 600px) {
+    font-size: 80px;
+    margin: 0em;
+  }
+`
 
 export default function Header() {
   return (
     <header>
       <Link to="/">
-        <h1>Kelly & Hunter</h1>
+        <Title>Kelly & Hunter</Title>
       </Link>
     </header>
   )
